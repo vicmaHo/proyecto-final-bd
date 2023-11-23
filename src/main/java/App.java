958a0f39f178ee7b088621e5eb2c5a35e0ff79fd
Victor Hernandez <victormanu968@gmail.com>
2023-11-23 18:37:11 -0500

    view test

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,7 @@
-package com.prendas;
+
+import javax.swing.JFrame;
+
+
 
 /**
  * Hello world!
@@ -9,5 +12,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        JFrame vista = new view.viewTest();
+        vista.setVisible(true);
     }
 }
